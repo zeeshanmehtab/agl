13926cf4a93b7a9f675ccaa77b5c379491f2fcae
Zeeshan Mehtab <zeeshan.mehtab1@gmail.com>
2025-05-03 12:22:13 +1000

    Initial Commit - Module Declaration

---
+++ SalesFunnel/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category    AGL
+ * @package     AGL_SalesFunnel
+ * @author      Zeeshan Mehtab <zeeshan.mehtab1@gmail.com>
+ * @copyright   Copyright (c) 2025 AGL (https://www.agl.com.au)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'AGL_SalesFunnel',
+    __DIR__
+); 
